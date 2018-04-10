516ed8138af248b8375b603c19ee8cbcc9a399a8
lixiaowei6 <!Zu6j3GTg7W>
2018-04-10 19:35:05 +0800

    add feature-2 on feature-2 branch

--- MainActivity.java
+++ MainActivity.java
@@ -41,4 +41,17 @@ public class MainActivity extends AppCompatActivity {
             btn_feature_1.setText(R.string.feature_1);
         }
     }
+
+    /**
+     * feature-2
+     * display HelloAndroid
+     * @param view
+     */
+    public void feature2(View view) {
+        if (btn_feature_2.getText().toString().equals("功能2")) {
+            btn_feature_2.setText(R.string.feature_2_dis);
+        } else {
+            btn_feature_2.setText(R.string.feature_2);
+        }
+    }
 }
