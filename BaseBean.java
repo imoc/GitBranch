281ea0066d43cc9b0766340e5694e83227292b17
lixiaowei6 <!Zu6j3GTg7W>
2018-04-10 19:22:03 +0800

    tag021

---
+++ BaseBean.java
@@ -0,0 +1,34 @@
+package com.jd.shop.gitbranch;
+
+/*
+
+ * -----------------------------------------------------------------
+
+ * Copyright (C) 2018-2021, by your company, All rights reserved.
+
+ * -----------------------------------------------------------------
+
+ *
+
+ * File: BaseBean.java
+
+ * Author: lixiaowei6
+
+ * Email: lixiaowei6@jd.com
+
+ * Version: V100R001C01
+
+ * Create: 2018/4/10 19:19
+
+ *
+
+ * Changes (from 2018/4/10)
+
+ * -----------------------------------------------------------------
+
+ * 2018/4/10 : Create BaseBean.java (lixiaowei6);
+
+ * -----------------------------------------------------------------
+
+ */public class BaseBean {
+}
